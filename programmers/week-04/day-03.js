08a027ec5d8c28cfeff388a9cfdcb8d4dda143d4
rea <woohr1109@gmail.com>
2021-11-24 10:42:08 +0900

    2021-11-24 : [update] programmers/week-04/day-03 (mento님 코드)

--- programmers/week-04/day-03.js
+++ programmers/week-04/day-03.js
@@ -8,3 +8,12 @@ function solution(arr1, arr2) {
   }
   return result;
 }
+
+function mento(arr1, arr2) {
+  let result = arr1.map((num1, i) => {
+    return num1.map((num2, j) => {
+      return num2 + arr2[i][j];
+    });
+  });
+  return result;
+}
