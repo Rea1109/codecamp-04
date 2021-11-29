a8dc66a84c0b281d6dd770240329b3eca9d7f64d
rea <woohr1109@gmail.com>
2021-11-29 11:10:31 +0900

    2021-11-29 : [modify] programmers/week-05_ref code 추가

--- programmers/week-05/day-01.js
+++ programmers/week-05/day-01.js
@@ -1,16 +1,20 @@
 //완주하지 못한 선수
 function solution(participant, completion) {
-  var answer = "";
-
   participant.sort();
   completion.sort();
 
   for (let i = 0; i < participant.length; i++) {
-    if (participant[i] != completion[i]) {
-      answer = participant[i];
-      break;
+    if (participant[i] !== completion[i]) {
+      return participant[i];
     }
   }
+}
 
-  return answer;
+function solution(participant, completion) {
+  participant.sort();
+  completion.sort();
+
+  return participant.filter((name, idx) => {
+    return name !== completion[idx];
+  })[0];
 }
